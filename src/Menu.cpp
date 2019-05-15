45eac8d156fb8fed5c30b569b69baab503993ac5
Raian Moretti <raian.moretti.eletro@gmail.com>
2019-05-15 17:22:36 -0300

    Menu Funcionando

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -7,19 +7,19 @@ Menu::Menu(float width, float height)
         cerr<<"Erro ao ler fonte"<<endl;
     }
     menu_text[0].setFont(font);
-    menu_text[0].setColor(sf::Color::White);
+    menu_text[0].setColor(sf::Color::Red);
     menu_text[0].setString("Jogar");
-    menu_text[0].setPosition(sf::Vector2f(width/2,(NUM_ITEMS+1)/4 * 1));
+    menu_text[0].setPosition(sf::Vector2f(600/2 -50,((NUM_ITEMS+1)/4) * 100));
 
     menu_text[1].setFont(font);
     menu_text[1].setColor(sf::Color::White);
     menu_text[1].setString("Opcoes");
-    menu_text[1].setPosition(sf::Vector2f(width/2,(NUM_ITEMS+1)/4 * 2));
+    menu_text[1].setPosition(sf::Vector2f(600/2 -50,((NUM_ITEMS+1)/4) * 200));
 
     menu_text[2].setFont(font);
     menu_text[2].setColor(sf::Color::White);
     menu_text[2].setString("Sair");
-    menu_text[2].setPosition(sf::Vector2f(width/2,(NUM_ITEMS+1)/4 * 3));
+    menu_text[2].setPosition(sf::Vector2f(600/2 -50,((NUM_ITEMS+1)/4) * 300));
 
     selectedItem = 0;
 }
