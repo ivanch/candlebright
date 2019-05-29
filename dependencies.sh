c3d056249f80a797282ccb359e84f23ad122bb52
Heitor Tonel Ventura <heitor@lap3d.com.br>
2019-05-29 02:06:08 -0300

    fix fatal error, olhem rapido antes que exploda

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,13 @@
-wget https://www.sfml-dev.org/files/SFML-2.5.1-linux-gcc-64-bit.tar.gz
-tar -xvf SFML-2.5.1-linux-gcc-64-bit.tar.gz
-mv SFML-2.5.1 sfml
-rm SFML-2.5.1-linux-gcc-64-bit.tar.gz
+#!/bin/bash
+
+[ ! -d ./obj ] && mkdir obj
+[ ! -d ./bin ] && mkdir bin
+
+if [ ! -d sfml ]
+then
+  echo "Instalando dependências =)"
+  wget https://www.sfml-dev.org/files/SFML-2.5.1-linux-gcc-64-bit.tar.gz
+  tar -xvf SFML-2.5.1-linux-gcc-64-bit.tar.gz
+  mv SFML-2.5.1 sfml
+  rm SFML-2.5.1-linux-gcc-64-bit.tar.gz
+fi
